c52123a282fa7cd41af758a4edaeb8606c00290e
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-01-07 20:11:58 +0100

    Params and searchParams added

--- src/app/(marketing)/customers/page.tsx
+++ src/app/(marketing)/customers/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
 function Page() {
-	return <div>Customers</div>;
+	return (
+		<div>
+			<h1>Welcome home!</h1>
+			<h2>
+				<Link href="/customers/blog">Blog</Link>
+			</h2>
+			<h2>
+				<Link href="/customers/products">Products</Link>
+			</h2>
+			<h2>
+				<Link href="/customers/products3" replace>
+					Products
+				</Link>
+			</h2>
+		</div>
+	);
 }
 
 export default Page;
