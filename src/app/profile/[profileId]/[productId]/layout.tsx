60e3b10e2300bdd01a7008b7665f40287ac2d836
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-01-10 19:51:19 +0100

    error-wrapper and global-error added to repository

--- src/app/profile/[profileId]/[productId]/layout.tsx
+++ src/app/profile/[profileId]/[productId]/layout.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { counters } from "sharp";
 
+const getRandomInt = (count: number) => {
+	return Math.floor(Math.random() * count);
+};
 export default function ProductDetail({
 	children
 }: {
 	children: React.ReactNode;
 }) {
+	const random = getRandomInt(2);
+	if (random === 1) {
+		throw new Error("Error loading reviews");
+	}
 	return (
 		<>
 			<h2>Featured products</h2>
