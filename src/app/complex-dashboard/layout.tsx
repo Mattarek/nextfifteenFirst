5ad0315651a6022796c179dc795a750d3acce1da
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-01-12 15:44:17 +0100

    Dynamic routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -4,22 +4,29 @@ export default ({
 	children,
 	users,
 	revenue,
-	notifications
+	notifications,
+	login
 }: {
 	children: React.ReactNode;
 	users: React.ReactNode;
 	revenue: React.ReactNode;
 	notifications: React.ReactNode;
-}) => (
-	<div>
-		<div>{children}</div>
-		<div style={{ display: "flex" }}>
-			<div style={{ display: "flex", flexDirection: "column" }}>
-				<div>{users}</div>
-				<div>{revenue}</div>
+	login: React.ReactNode;
+}) => {
+	const isLoggedIn = true;
+
+	return isLoggedIn ? (
+		<div>
+			<div>{children}</div>
+			<div style={{ display: "flex" }}>
+				<div style={{ display: "flex", flexDirection: "column" }}>
+					<div>{users}</div>
+					<div>{revenue}</div>
+				</div>
+				<div style={{ display: "flex", flex: 1 }}>{notifications}</div>
 			</div>
-			<div style={{ display: "flex", flex: 1 }}>{notifications}</div>
 		</div>
-		{/*<UserAnalytics />*/}
-	</div>
-);
+	) : (
+		login
+	);
+};
